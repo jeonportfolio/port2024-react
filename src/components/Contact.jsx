a44f086c1c4572ed9d177d4771156f56eb1ba66d
jeons <etoile929@naver.com>
2024-03-03 17:02:49 +0900

    리액트 포트폴리오 커밋완료

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+
+const contactText = [
+  {
+      link: "",
+      title: "KAKAO : ",
+  },
+  {
+      link: "mailto:etoile929@naver.com",
+      title: "mail : etoile929@naver.com",
+  },
+];
+
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section id="contact">
+        <div className="contact__inner">
+            <h2 className="contact__title">Contact</h2>
+            <div className="contact__lines" aria-hidden="true">
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+            </div>
+            <div className="contact__text">
+                <div className="text">
+                    {contactText.map((contact, key) => (
+                          <div key={key}>
+                              <a 
+                                  href={contact.link} 
+                                  target="_blank" 
+                                  rel="noopener noreferrer"
+                              >
+                                  {contact.title}
+                              </a>
+                                  {/* noopener noneferrer는 데이터 유출을 방지하는 것  */}
+                            </div>
+                    ))}
+                    
+                   
+                </div>
+            </div>
+            <div className="contact__lines bottom" aria-hidden="true">
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+            </div>
+        </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
